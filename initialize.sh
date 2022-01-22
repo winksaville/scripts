d8e89bbdddaaf653d8c0ce7acf370a8ee4002a3f
Wink Saville <wink@saville.com>
2022-01-21 17:40:38 -0800

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -9,8 +9,14 @@ cp ~/scripts/bashrc ~/.bashrc
 cp ~/scripts/bash_profile ~/.bash_profile
 cp ~/scripts/gitconfig ~/.gitconfig
 cp ~/scripts/vimrc ~/.vimrc
-cp -r ~/scripts/vim ~/.vim
+cp -r ~/scripts/vim ~/.vim/
 cp -r ~/scripts/nvim ~/.config/
-cp -r ~/scripts/ssh/config ~/.ssh/config
+
+cp ~/scripts/ssh/config ~/.ssh/
 chmod 600 ~/.ssh/config
+
+mkdir -p ~/bin
+cp ~/scripts/update-lighthouse.sh ~/bin/
+cp ~/scripts/remove-all-from-docker.sh ~/bin/
+cp ~/scripts/screensaver-lock-display-off ~/bin/
 
