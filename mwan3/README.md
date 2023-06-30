eb5e58fefd65bcd14ab14ca5fdae3ba224b19c2f
Wink Saville <wink@saville.com>
2023-06-30 12:06:23 -0700

    chore: Improve my mwan3 code
    Biggest change was using `nohup` and mwan3.process.sh to get better background
    processing but that didn't work out, see comment. But left it in incase I need it in the
    future.
    
     - mail-wink.sh add case statement to handle two or three parameters
     - mwan3.user have it call mwan3.process.sh to simplify it and allow for use of `nohup`
     - mwan3.process.sh does the actualy processing of the ACTION and uses a case
      statement to make it cleaner and allow action specific handling.

--- mwan3/README.md
+++ mwan3/README.md
@@ -5,6 +5,10 @@ See https://openwrt.org/docs/guide-user/network/wan/multiwan/mwan3
 mwan3.user       A script that lives in /etc/ and is automatially
                  invoked by mwan3 when an ACTION occurs
 
+mwan3.process.sh Invoked by mwan3.user so the processing done by
+                 mwan3.user could be invoked with `nohup`. Turns
+                 out that didn't work well, see mwan3.user.
+
 date-utc-nlf.sh  A script that is used by mwan3.user and lives in
                  /root it prints the date in utc without a linefeed
 
--- mwan3/mail-wink.sh
+++ mwan3/mail-wink.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
-# $1 is email@address.com
-# $2 is subject
-# $3 is message
+# Expecting two or three parameters
+#   $1 is email@address.com
+#   $2 is message
+#
+#   $1 is email@address.com
+#   $2 is subject
+#   $3 is message
 
-mailsend -smtp smtp.gmail.com \
--port 587 \
--starttls \
--f wink@saville.com \
-+cc +bc \
--auth-login \
--user wink@saville.com \
--pass 'hlkdytgjowvkayki' \
--t "$1" \
--sub "$2" \
--M "$3"
+COMMAND="mailsend -smtp smtp.gmail.com -port 587 -starttls -f wink@saville.com +cc +bc -auth-login -user wink@saville.com -pass hlkdytgjowvkayki -t $1"
+
+echo COMMAND=$COMMAND -M "$2"
+
+case $# in
+  2)
+    $COMMAND -sub " " -M "$2"
+    STATUS=$?
+    ;;
+  3)
+    $COMMAND -sub "$2" -M "$3"
+    STATUS=$?
+    ;;
+  *)
+    echo "Expecting 2 parameters"
+    echo "  $0 <address> <message>"
+    echo "or 3 parameters"
+    echo "  $0 <address> <subject> <message>"
+    STATUS=255
+esac
+
+exit $STATUS
