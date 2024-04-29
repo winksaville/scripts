8121321e91ae6c2ed6139913fbcae9c122106c98
Wink Saville <wink@saville.com>
2024-04-29 12:36:10 -0700

    feat: Update doc for error flags

--- mbvc.sh
+++ mbvc.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-# Disable error options so we don't have to "install existing package".
-# TODO: Actually we should test the "Already up to date" warning from the
-#       git pull and return from `mp()` early.
+# More strigent errors handling
+# -E      If set, any trap on ERR is inherited by shell functions, command substitutions, and commands executed in a subshell environment.
+#         The ERR trap is normally not inherited in such cases.
+# -e      Exit immediately if a pipeline (which may consist of a single simple command), a list, or a compound command (see SHELL  GRAMMAR
+#         above), exits with a non-zero status. (Lots of exceptions see `man bash`)
+# -u      Treat  unset variables and parameters other than the special parameters "@" and "*" as an error when performing parameter expansion.
+# -o      pipefail: When set, the return value of a pipeline is the value of the last (rightmost) command to exit
+#         with a non-zero status, or zero if all commands in the pipeline exit successfully.
 #set -Eeuo pipefail
 
 # Enable debug
