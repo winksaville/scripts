65722c26fc4570da622e7c09179d5529637bbdc5
Wink Saville <wink@saville.com>
2024-07-03 13:33:19 -0700

    feat: Changed dir for api-token and some clean up
    Renamed the location rather than ~/api-token/ it is now
    ~/api-tokens/mainnet-validators.
    
    The cleanup includes some renaming and adding dir and port vars

--- vm-move.sh
+++ vm-move.sh
@@ -7,35 +7,46 @@ set -Eeuo pipefail
 # Enable debug
 #set -x
 
-echo "\$#=$#"
+# Some default assumptions maybe make as optional parameters in the future.
+port=5062
+dir=~/api-tokens/mainnet-validators
+
+# debug
+#echo "\$#=$#"
+
 if (( $# < 2 )) then
     filename=$(basename $0)
-    echo "Usage: $filename <src_vc> <dest_vc> <other>"
-    echo "  src_vc:    Source domain assumes port 5062 example: name@computerA"
-    echo "  dest_vc:   Destination domain assumes port 5062 example: name@computerB"
+    echo "Usage: $filename <src_vc> <dst_vc> <other>"
+    echo "  src_vc:    Source domain assumes port $port example: name@computerA"
+    echo "  dst_vc:    Destination domain assumes port $port example: name@computerB"
     echo "  other:     All other parameters are passed as is, these could include:"
     echo "               --validators {all|publlic_keys}"
     echo "               --count <vc_count>"
     echo "             Use 'lighthouse vm move --help' for other parameters"
     echo
-    echo "  Assumes:   src-vc-token = ~/api-token/\$src_vc"
-    echo "             dst-vc-token = ~/api-token/\$dest_vc"
+    echo "  Assumes:   tokens are in directory = $dir/"
     exit 1
 fi
 
 src_vc=$1
 shift
-dest_vc=$1
+dst_vc=$1
 shift
 other=$@
 
-if [ ! -f ~/api-token/$src_vc.api-token.txt ]; then echo "~/api-token/$src_vc.api-token.txt NOT FOUND"; exit 1; fi
-if [ ! -f ~/api-token/$dest_vc.api-token.txt ]; then echo "~/api-token/$dest_vc.api-token.txt NOT FOUND"; exit 1; fi
+if [ ! -f $dir/$src_vc.api-token.txt ]; then echo "$dir/$src_vc.api-token.txt NOT FOUND"; exit 1; fi
+if [ ! -f $dir/$dst_vc.api-token.txt ]; then echo "$dir/$dst_vc.api-token.txt NOT FOUND"; exit 1; fi
+
+# debug
+#echo src_vc="$src_vc" dst_vc="$dst_vc" other="$other"
+#echo port="$port"
+#echo dir="$dir"
+#ls $dir
 
 lighthouse vm move \
-  --src-vc-token ~/api-token/$src_vc.api-token.txt \
-  --src-vc-url http://kendall@$src_vc:5062 \
-  --dest-vc-token ~/api-token/$dest_vc.api-token.txt \
-  --dest-vc-url http://kendall@$dest_vc:5062 \
+  --src-vc-token $dir/$src_vc.api-token.txt \
+  --src-vc-url http://kendall@$src_vc:$port \
+  --dest-vc-token $dir/$dst_vc.api-token.txt \
+  --dest-vc-url http://kendall@$dst_vc:$port \
   $other
 
