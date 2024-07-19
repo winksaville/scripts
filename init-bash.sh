4efe434e73a3acae6ae857556f1c706eaf3bfe46
Wink Saville <wink@saville.com>
2024-07-19 15:54:43 -0700

    feat: Add copying editorconfig to ~/.editorconfig
    ~/.editorconfig is used by shfmt, see https://editorconfig.org/ and
    `man shfmt` for more info.

--- init-bash.sh
+++ init-bash.sh
@@ -13,7 +13,10 @@ cp ~/scripts/bash_profile ~/.bash_profile
 cp ~/scripts/vimrc ~/.vimrc
 cp -r ~/scripts/vim ~/.vim/
 cp -r ~/scripts/nvim ~/.config/
-if [ ! $(command -v vi) ]; then sudo ln -s /usr/bin/vim /usr/bin/vi; fi
+if [ ! "$(command -v vi)" ]; then sudo ln -s /usr/bin/vim /usr/bin/vi; fi
+
+# Used by shfmt
+cp ~/scripts/editorconfig ~/.editorconfig
 
 #echo "init-bash.sh:-"
 
